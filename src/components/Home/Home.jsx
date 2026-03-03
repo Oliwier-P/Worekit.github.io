f6ab74d7b30c16b56f4090716d9e93dcc6cef98b
Oliwier <worekit@gmail.com>
2026-03-03 22:41:54 +0100

    personal info

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,14 +6,16 @@ export const Home = ({ innerRef }) => {
         <div className="home__header">
             <div className="home__name">
                 <span>Oliwier Paśko</span>
-                <span className="home__name-highlight">Developer</span>
+                <span className="home__name-highlight">Full-Stack Developer</span>
             </div>
             <div className="home__image"></div>
         </div>
 
         <div className="home__title">O mnie</div>
         <div className="home__description">
-            Loqweqsdvjska wqjdfnjk s qsjfv jqsdfjv qwerj qwadjb fvashj n asdjasd  asd fsadf asdfasdfasds
+            Junior Full-Stack Developer pracujący z React, TypeScript i Node.js.
+            Od 6 lat rozwijam się w programowaniu, od 3 lat tworzę projekty full-stack.
+            Skupiam się na czystym kodzie i budowie nowoczesnych aplikacji webowych w czasie rzeczywistym.
         </div>
     </div>
 }
