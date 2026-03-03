85dacf2fe6b5281d56beadb18abf2af9f0e05398
Oliwier <worekit@gmail.com>
2026-03-03 18:22:31 +0100

    contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,23 @@
 import "./Contact.scss"
 
 export const Contact = () => {
-    return <div className="contact"></div>
+    return <div className="contact">
+        <div className="contact-item">
+            <div className="contact-item__logo"></div>
+            <div className="contact-item__text">Email</div>
+            <div className="contact-item__data">oliwierpasko25@gmail.com</div>
+        </div>
+
+        <div className="contact-item">
+            <div className="contact-item__logo"></div>
+            <div className="contact-item__text">Telefon</div>
+            <div className="contact-item__data">732 253 341</div>
+        </div>
+
+        <div className="contact-item">
+            <div className="contact-item__logo"></div>
+            <div className="contact-item__text">GitHub</div>
+            <div className="contact-item__data">Oliwier-P</div>
+        </div>
+    </div>
 }
